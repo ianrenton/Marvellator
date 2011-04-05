efeb8c3eda546c8cb4e3db039cd96faca8e4f8b3
Ian Renton <ian@onlydreaming.net>
2011-04-05 01:38:51 -0700

    Added 'life imitates art' line.

--- index.php
+++ index.php
@@ -14,7 +14,8 @@
             h2 {margin-bottom:3em;}
             form {margin: 4em 0;}
             span.title { background-color:red; color:white; font-family:"Impact",sans-serif; text-transform: uppercase; font-weight: normal; font-size:2em; padding:5px 10px;}
-            ul.share{margin:2em 0 0 0; padding:0; width:100%; text-align: center;}
+            p.bottompad{margin-bottom:2.5em;}
+            ul.share{margin-bottom:2.5em; padding:0; width:100%; text-align: center;}
             ul.share li {display: inline; list-style-type: none; margin:0; padding: 0 20px 0 20px;}
         </style>
     </head>
@@ -35,12 +36,13 @@
         </h1>
         <form submit=""><input type="submit" value="Generate another!" /></form>
 
-        <p>The Mavellator is a random comic name generator, written by <a href="http://onlydreaming.net/about-me">this guy</a>. It is not affiliated with Marvel Comics (as if there was any doubt in your mind).</p>
+        <p class="bottompad">The Mavellator is a random comic name generator, written by <a href="http://onlydreaming.net/about-me">this guy</a>. It is not affiliated with Marvel Comics (as if there was any doubt in your mind).</p>
         <ul class="share">
             <li><a href="http://twitter.com/home?status=Behold the #Marvellator! http://onlydreaming.net/marvellator">Tweet it</a></li>
             <li><a href="http://www.facebook.com/share.php?u=http://onlydreaming.net/marvellator">Facebookify it</a></li>
             <li><a href="mailto:marvellator@onlydreaming.net?subject=Marvellator%20Suggestion">Suggest more things</a></li>
             <li><a href="mailto:someone%20who%20gives%20a%20damn?subject=Paging%20the%20Internet%20Police!">Complain about it</a></li>
         </ul>
+        <p class="bottompad"><em>Life Imitates Art</em>, or, "things the Marvellator has produced that, oddly, actually exist":<br/><a href="http://gunshowcomic.com/133">Adventures of Tiny Rorschach</a> | <a href="http://bit.ly/gYh9A0">Transgender Justice League</a></p>
     </body>
 </html>
